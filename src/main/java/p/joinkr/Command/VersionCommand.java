bd06845937ec9777c0accd54377629b8053c2414
Spinnin <ahmedomrane398@gmail.com>
2024-04-13 14:02:03 +0200

    Version

--- src/main/java/p/joinkr/Command/VersionCommand.java
+++ src/main/java/p/joinkr/Command/VersionCommand.java
@@ -1,4 +1,42 @@
 package p.joinkr.Command;
 
-public class VersionCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import p.joinkr.utils.MessageUtils;
+
+public class VersionCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Este comando solo puede ser ejecutado por un jugador.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        // Construir el mensaje de varias líneas
+        TextComponent message = new TextComponent(MessageUtils.sendCenteredMessage("§x§B§8§5§7§F§Bᴊ§x§B§9§5§E§F§Bᴏ§x§B§A§6§6§F§Cɪ§x§B§B§6§D§F§Cɴ §x§B§B§7§4§F§Cᴋ§x§B§C§7§C§F§Dᴀ§x§B§D§8§3§F§Dʀ§x§B§E§8§A§F§Dᴍ§x§B§F§9§2§F§Eᴀ§x§C§0§9§9§F§Eɴ§x§C§0§A§0§F§Eᴄ§x§C§1§A§8§F§Fᴏ§x§C§2§A§F§F§Fs\n \n§fHᴀᴢ ᴄʟɪᴄ ᴘᴀʀᴀ ᴀᴄᴄᴇᴅᴇʀ ᴀ ɪɴғᴏʀᴍᴀᴄɪᴏɴ ᴀᴄᴛᴜᴀʟɪᴢᴀᴅᴀ ʏ ɴᴜᴇᴠᴀs ᴀᴄᴛᴜᴀʟɪᴢᴀᴄɪᴏɴᴇs.\n \n§7Developer ©Spinnin34"));
+        message.addExtra("Web Oficial ");
+
+        // Componente de texto para el enlace
+        TextComponent link = new TextComponent("sitio web");
+        link.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://example.com"));
+        link.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponent[]{
+                new TextComponent("¡Haz clic aquí para visitar nuestro sitio web!")
+        }));
+
+        // Agregar el enlace al mensaje
+        message.addExtra(link);
+        message.addExtra(" para obtener más información.");
+
+        // Enviar el mensaje al jugador
+        player.spigot().sendMessage(message);
+
+        return true;
+    }
 }
--- src/main/java/p/joinkr/Join_KR.java
+++ src/main/java/p/joinkr/Join_KR.java
@@ -2,8 +2,9 @@ package p.joinkr;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import p.joinkr.Command.VersionCommand;
+import p.joinkr.events.OnPlayerDisconnect;
 import p.joinkr.events.OnPlayerJoin;
 
 public final class Join_KR extends JavaPlugin {
@@ -19,11 +20,15 @@ public final class Join_KR extends JavaPlugin {
         //Saves the configuration file
         this.saveDefaultConfig();
 
-        //Registers the player join event defined in OnPlayerJoin();
+        //Registers the player join event defined in OnPlayerJoin()
         getServer().getPluginManager().registerEvents(new OnPlayerJoin(this), this);
+        getServer().getPluginManager().registerEvents(new OnPlayerDisconnect(this), this);
+
+        //Commandos
+        getCommand("version").setExecutor(new VersionCommand());
 
         //Woo hoo no errors!
-        console.sendMessage(ChatColor.GREEN + "Title on join working as expected :)");
+        console.sendMessage(ChatColor.GREEN + "El título al unirse funciona como se esperaba :)");
 
     }
 
